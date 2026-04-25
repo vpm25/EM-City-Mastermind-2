24f3c14fab38d02f8bc947d5c59944af38b6a330
vpm25 <vero.vpm25@gmail.com>
2026-04-24 17:51:23 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import dynamic from "next/dynamic";
-
 const SurveyApp = dynamic(() => import("../components/SurveyApp"), { ssr: false });
-
 export default function Home() {
   return <SurveyApp />;
 }
